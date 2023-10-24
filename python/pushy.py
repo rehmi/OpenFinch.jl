7703a5241509a06753dab027a96a5dede24f8b15
Rehmi Post <rehmi.post@gmail.com>
2023-10-23 23:32:17 -0700

    globalized image parameters in pushy.py

--- python/pushy.py
+++ python/pushy.py
@@ -9,9 +9,9 @@ import time
 import threading
 import io
 
-def generate_image(brightness, contrast, gamma):
+def generate_image(brightness, contrast, gamma, width=1600, height=1200):
 	# Create a random RGB image
-	img = Image.fromarray(np.random.randint(0, 256, (1200, 1600, 3), dtype=np.uint8))
+	img = Image.fromarray(np.random.randint(0, 256, (height, width, 3), dtype=np.uint8))
 
 	# Adjust brightness
 	enhancer = ImageEnhance.Brightness(img)
@@ -33,13 +33,16 @@ def generate_image(brightness, contrast, gamma):
 	img_base64 = base64.b64encode(rawBytes.read())
 	return 'data:image/jpeg;base64,' + str(img_base64, 'utf-8')
 
-def push_image(brightness, contrast, gamma):
-	img_str = generate_image(brightness, contrast, gamma)
+
+brightness, contrast, gamma = (0.5, 0.5, 1.0)
+
+def push_image(brightness, contrast, gamma, height=12, width=16):
+	img_str = generate_image(brightness, contrast, gamma, height=height, width=width)
 	socketio.emit('image_response', {'image': img_str})
 
-def push_random_image():
+def push_random_image(height=1200, width=1600):
 	global brightness, contrast, gamma
-	img_str = generate_image(brightness, contrast, gamma)
+	img_str = generate_image(brightness, contrast, gamma, height=height, width=width)
 	socketio.emit('image_response', {'image': img_str})
 
 app = Flask(__name__)
@@ -48,46 +51,52 @@ socketio = SocketIO(app)
 
 client_connected = False
 
-brightness, contrast, gamma = (0.5, 0.5, 1.0)
+img_height, img_width = 12, 16
 
 @app.route('/')
 def index():
-	return render_template('pushy.html')
+    print("*** index")
+    return render_template('pushy.html')
 
 @socketio.on('control_change')
 def handle_control_change(message):
-	global brightness, contrast, gamma
-	brightness = float(message.get('brightness'))
-	contrast = float(message.get('contrast'))
-	gamma = float(message.get('gamma'))
-	# print(f"got control_change, brightness={brightness}, contrast={contrast}, gamma={gamma}")
-	push_random_image()
+    print("*** handle_control_change")
+    global brightness, contrast, gamma
+    brightness = float(message.get('brightness'))
+    contrast = float(message.get('contrast'))
+    gamma = float(message.get('gamma'))
+    print(f"got control_change, brightness={brightness}, contrast={contrast}, gamma={gamma}")
+    push_random_image(height=img_height, width=img_width)
 
 @socketio.on('image_request')
 def handle_image_request(message):
-	brightness = float(message.get('brightness'))
-	contrast = float(message.get('contrast'))
-	gamma = float(message.get('gamma'))
-	push_image(brightness, contrast, gamma)
+    print("*** handle_image_request")
+    brightness = float(message.get('brightness'))
+    contrast = float(message.get('contrast'))
+    gamma = float(message.get('gamma'))
+    push_image(brightness, contrast, gamma, height=img_height, width=img_width)
 
 @socketio.on('connect')
 def handle_connect():
-	def generate_images():
-		global client_connected
-		while client_connected:
-			push_random_image()
-			time.sleep(1)
+    print("*** handle_connect")
+    def generate_images():
+        print("*** generate_images in handle_connect")
+        global client_connected
+        while client_connected:
+            push_random_image(height=12, width=16)
+            time.sleep(1)
 
-	global client_connected
-	if not client_connected:
-		client_connected = True
-		socketio.start_background_task(generate_images)
+    global client_connected
+    if not client_connected:
+        client_connected = True
+        socketio.start_background_task(generate_images)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-	global client_connected
-	client_connected = False
-	yield()
+    print("*** handle_disconnect")
+    global client_connected
+    client_connected = False
+    yield()
 
 if __name__ == '__main__':
 	socketio.run(app, port=8086, debug=True)
