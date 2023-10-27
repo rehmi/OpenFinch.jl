f1d0b67bc5968e21ac2abec21761f97fa1bbd01b
Rehmi Post <rehmi.post@gmail.com>
2023-10-26 17:53:18 -0700

    client and server now transport images in binary

--- python/vanilla.jl
+++ python/vanilla.jl
@@ -8,22 +8,53 @@ using Base64
 using FileIO
 
 function generate_image(brightness, contrast, gamma; width=16, height=12)
-    img = rand(RGB, height, width)
+    img = rand(Gray, height, width)
     # img = adjust_gamma(img, gamma)
     # img = adjust_contrast(img, contrast)
     # img = adjust_brightness(img, brightness)
-	return img
+    return img
 end
 
-function image_to_string(img)
+function image_to_binary(img; quality=nothing)
     img_io = IOBuffer()
-    save(Stream{format"JPEG"}(img_io), img)
-    img_base64 = base64encode(img_io.data)
-    return "data:image/jpeg;base64," * img_base64
+    if quality != nothing
+        save(Stream{format"JPEG"}(img_io), img, quality=quality)
+    else
+        save(Stream{format"PNG"}(img_io), img)
+    end
+    return img_io.data
+end
+
+function image_to_string(img; quality::Int=nothing)
+	img_io = IOBuffer()
+	if quality != nothing
+		save(Stream{format"JPEG"}(img_io), img, quality=quality)
+		str = "data:image/jpeg;base64," * base64encode(img_io.data)
+	else
+		save(Stream{format"PNG"}(img_io), img)
+		str = "data:image/png;base64," * base64encode(img_io.data)
+	end
+	return str
+end
+
+const use_jpeg = Ref(true)
+const quality = Ref(75)
+
+function send_image(ws=nothing; height=12, width=16, quality=quality[])
+    img = generate_image(brightness, contrast, gamma, height=height, width=width)
+    img_bin = image_to_binary(img, quality=quality)
+
+    # Send JSON message first
+    msg = JSON.json(Dict("image_response" => Dict("image" => "next")))
+    send(ws, msg)
+
+    # Then send binary data
+    send(ws, img_bin)
 end
 
 brightness, contrast, gamma = 0.5, 0.5, 1.0
-img_height, img_width = 12, 16
+img_height = Ref(1200)
+img_width = Ref(1600)
 
 connected = Set()
 
@@ -31,29 +62,21 @@ function handle_message(ws::WebSockets.WebSocket, data::String)
     message = JSON.parse(data)
     control_change = get(message, "control_change", Dict())
     image_request = get(message, "image_request", Dict())
-    global brightness, contrast, gamma
 
+    global brightness, contrast, gamma
     brightness = parse(Float64, get(control_change, "brightness", get(image_request, "brightness", string(brightness))))
     contrast = parse(Float64, get(control_change, "contrast", get(image_request, "contrast", string(contrast))))
     gamma = parse(Float64, get(control_change, "gamma", get(image_request, "gamma", string(gamma))))
 
-	@info "in handle_message: brightness=$brightness, contrast=$contrast, gamma=$gamma"
-
-	img = generate_image(brightness, contrast, gamma, height=img_height, width=img_width)
-	img_str = image_to_string(img)
-
-	msg = JSON.json(Dict("image_response" => Dict("image" => img_str)))
-
-	# @info "sending message $msg"
-    send(ws, msg)
+	send_image(ws, height=img_height[], width=img_width[])
 end
 
 function start_server()
     return HTTP.listen!(Sockets.localhost, 8000; verbose=true) do http::HTTP.Streams.Stream
         req = http.message
-		@info req
+		# @info req
         if HTTP.header(req, "Upgrade") == "websocket"
-			@info "upgrading to websocket"
+			# @info "upgrading to websocket"
             WebSockets.upgrade(http) do ws
 				try
 					push!(connected, ws)
@@ -66,13 +89,13 @@ function start_server()
 				end
             end
         else
-			@info req.target
+			# @info req.target
             if req.target == "/"
-				@info "responding with 200 vanilla.html"
+				# @info "responding with 200 vanilla.html"
 				status = 200
 				body = read("vanilla.html")
             else
-                @info "responding with 404"
+                # @info "responding with 404"
 				status = 404
 				body = ""
             end
@@ -83,18 +106,6 @@ function start_server()
 			write(http, body)
 			return
         end
-    end
-end
-
-function send_images()
-    while true
-        img_str = generate_image(brightness, contrast, gamma, height=img_height, width=img_width)
-        if !isempty(connected)
-            for ws in connected
-                send(ws, JSON.json(Dict("image_response" => Dict("image" => img_str))))
-            end
-        end
-        sleep(1)
     end
 end
 
