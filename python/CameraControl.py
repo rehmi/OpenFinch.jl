38a2d834e4e8cc02e27c649314964c56e23b60e3
Rehmi Post <rehmi.post@gmail.com>
2024-01-09 17:29:34 -0800

    factor out PiGPIOWave

--- python/CameraControl.py
+++ python/CameraControl.py
@@ -105,49 +105,65 @@ def start_pig(host="localhost", port=8888):
 		# raise Exception('Could not connect to pigpio')
 	return pig
 
+class PiGPIOWave:
+	def __init__(self, pig, **kwargs):
+		self.pig = pig
+		self.kwargs = kwargs
+		self.wave = []
+		self.id = self.generate_wave()
+
+	def generate_wave(self):
+		TRIG_IN = self.kwargs.get('TRIG_IN', 25)
+		TRIG_TIME = self.kwargs.get('TRIG_TIME', 0)
+		TRIG_OUT = self.kwargs.get('TRIG_OUT', 5)
+		TRIG_WIDTH = self.kwargs.get('TRIG_WIDTH', 50)
+		LED_IN = self.kwargs.get('LED_IN', 22)
+		LED_TIME = self.kwargs.get('LED_TIME', 5555)
+		LED_OUT = self.kwargs.get('LED_OUT', 17)
+		RED_OUT = self.kwargs.get('RED_OUT', 17)
+		GRN_OUT = self.kwargs.get('GRN_OUT', 27)
+		BLU_OUT = self.kwargs.get('BLU_OUT', 23)
+		LED_WIDTH = self.kwargs.get('LED_WIDTH', 500)
+		STROBE_IN = self.kwargs.get('STROBE_IN', 6)
+		WAVE_DURATION = self.kwargs.get('WAVE_DURATION', 16667)
+
+		for pin in [TRIG_OUT, RED_OUT, GRN_OUT, BLU_OUT, LED_OUT]:
+			self.pig.set_mode(pin, pigpio.OUTPUT)
+
+		for pin in [TRIG_IN, LED_IN, STROBE_IN]:
+			self.pig.set_mode(pin, pigpio.INPUT)
+
+		dtled = max(0, LED_TIME - TRIG_WIDTH - TRIG_TIME)
+		dtif = max(0, WAVE_DURATION - LED_TIME - LED_WIDTH - TRIG_TIME - TRIG_WIDTH)
+
+		LED_ON_HIGH = self.kwargs.get('LED_MASK', 1<<LED_OUT)
+		LED_ON_LOW = 0
+		LED_OFF_HIGH = 0
+		LED_OFF_LOW = self.kwargs.get('LED_MASK', 1<<LED_OUT)
+
+		self.wave.append(pigpio.pulse(0, 0, TRIG_TIME))
+		self.wave.append(pigpio.pulse(0, 1<<TRIG_OUT, TRIG_WIDTH))
+		self.wave.append(pigpio.pulse(1<<TRIG_OUT, 0, dtled))
+		self.wave.append(pigpio.pulse(LED_ON_HIGH, LED_ON_LOW, LED_WIDTH))
+		self.wave.append(pigpio.pulse(LED_OFF_HIGH, LED_OFF_LOW, dtif))
+		self.pig.wave_add_generic(self.wave)
+		return self.pig.wave_create()
+
+		def delete_wave(self):
+			self.pig.wave_delete(self.id)
+
+
 def trigger_wave_script(pig, **kwargs):
 	# Default values for the parameters
 	TRIG_IN = kwargs.get('TRIG_IN', 25)
-	TRIG_TIME = kwargs.get('TRIG_TIME', 0)
-	TRIG_OUT = kwargs.get('TRIG_OUT', 5)
-	TRIG_WIDTH = kwargs.get('TRIG_WIDTH', 50)
-	LED_IN = kwargs.get('LED_IN', 22)
-	LED_TIME = kwargs.get('LED_TIME', 5555)
-	LED_OUT = kwargs.get('LED_OUT', 17)
-	RED_OUT = kwargs.get('RED_OUT', 17)
-	GRN_OUT = kwargs.get('GRN_OUT', 27)
-	BLU_OUT = kwargs.get('BLU_OUT', 23)
-	LED_WIDTH = kwargs.get('LED_WIDTH', 500)
 	STROBE_IN = kwargs.get('STROBE_IN', 6)
-	WAVE_DURATION = kwargs.get('WAVE_DURATION', 16667)
 
-	for pin in [TRIG_OUT, RED_OUT, GRN_OUT, BLU_OUT, LED_OUT]:
-		pig.set_mode(pin, pigpio.OUTPUT)
-
-	for pin in [TRIG_IN, LED_IN, STROBE_IN]:
-		pig.set_mode(pin, pigpio.INPUT)
-
-	dtled = max(0, LED_TIME - TRIG_WIDTH - TRIG_TIME)
-	dtif = max(0, WAVE_DURATION - LED_TIME - LED_WIDTH - TRIG_TIME - TRIG_WIDTH)
-
-	LED_ON_HIGH = kwargs.get('LED_MASK', 1<<LED_OUT)
-	LED_ON_LOW	= 0
-	LED_OFF_HIGH = 0
-	LED_OFF_LOW = kwargs.get('LED_MASK', 1<<LED_OUT)
- 
-	wave = []	
-	wave.append(pigpio.pulse(0, 0, TRIG_TIME))
-	wave.append(pigpio.pulse(0, 1<<TRIG_OUT, TRIG_WIDTH))
-	wave.append(pigpio.pulse(1<<TRIG_OUT, 0, dtled))
-	wave.append(pigpio.pulse(LED_ON_HIGH, LED_ON_LOW, LED_WIDTH))
-	wave.append(pigpio.pulse(LED_OFF_HIGH, LED_OFF_LOW, dtif))
-	pig.wave_add_generic(wave)
-	wave_id = pig.wave_create()
+	wave = PiGPIOWave(pig, **kwargs)
 	
 	script = f"""
 	pads 0 16								# set pad drivers to 16 mA
 
-	lda {wave_id} sta p0
+	lda {wave.id} sta p0
 	lda {TRIG_IN} sta p1
 	lda {STROBE_IN} sta p2
 
@@ -211,6 +227,6 @@ def trigger_loop(pig, n=1000, t_min=2777, t_max=2778+8333, **kwargs):
 
 
 if __name__ == "__main__":
-   pig = start_pig()
-   trigger_loop(pig)
+	pig = start_pig()
+	trigger_loop(pig)
 
