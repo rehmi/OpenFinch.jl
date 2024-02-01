428e09f7d92c70aef2174672b007300588dd123f
Rehmi Post <rehmi.post@gmail.com>
2024-01-31 19:09:55 -0800

    BusVector and WaveVector are now private classes in WaveGen, and are now directly indexable/iterable/printable

--- OpenFinchServer/wavegen.py
+++ OpenFinchServer/wavegen.py
@@ -1,90 +1,8 @@
-import collections
-
-class BusVector:
-    def __init__(self, changes):
-        self.changes = changes
-        
-    def __getitem__(self, index):
-        return self.changes[index]
-
-    def __str__(self):
-        return '\n'.join([f"Time:  {time:5d}, Mask: {mask:032b}" for time, mask in self.generate()])
-
-    def __len__(self):
-        return len(self.changes)
-    
-    def generate(self):
-        changes = sorted(self.changes, key=lambda x: x[2])
-        bus_vector = []
-        current_time = 0
-        current_mask = 0
-        for bit_index, bit_value, time in changes:
-            delay = time - current_time
-            if delay < 0:
-                raise ValueError("Time of change is less than current time")
-            else:
-                bus_vector.append((current_time, current_mask))
-            current_time = time
-            if bit_value:
-                current_mask |=  (1 << bit_index)
-            else:
-                current_mask &= ~(1 << bit_index)
-                
-        # Include the final state at the end of simulation, after processing all changes.
-        bus_vector.append((current_time, current_mask))
-                
-        return bus_vector
-
-
-class WaveVector:
-    def __init__(self, changes):
-        self.changes = sorted(changes, key=lambda x: x[2])
-
-    def __str__(self):
-        return '\n'.join([f"Delay: {delay:5d},  Set: {set_mask:032b}, Clear: {clr_mask:032b}" for set_mask, clr_mask, delay in self.generate()])
-
-    def generate(self):
-        wave_vector = []
-        prev_time = 0
-        set_mask = 0
-        clr_mask = 0
-
-        # Iterate over sorted changes
-        for bit_index, bit_value, current_time in self.changes:
-            # Calculate delay since last change
-            delay = current_time - prev_time
-            
-            # If delay < 0, the changes are not properly sorted.
-            if delay < 0:
-                raise ValueError("Time of change is less than current time")
-            
-            # If there was a delay, record it and reset the masks
-            if delay > 0:
-                wave_vector.append((set_mask, clr_mask, delay))
-                set_mask = 0
-                clr_mask = 0
-
-            # Update masks based on the bit value
-            if bit_value:
-                set_mask |= (1 << bit_index)
-            else:
-                clr_mask |= (1 << bit_index)
-
-            # Update previous time
-            prev_time = current_time
-
-        # Include the final change if any mask is set
-        if set_mask or clr_mask:
-            wave_vector.append((set_mask, clr_mask, 0))
-
-        return wave_vector
-
-
 class WaveGen:
     def __init__(self):
         self._changes = []
 
-    def change_bit(self, bit_index: int, bit_value: int, time: int) -> None:
+    def change_bit(self, bit_index: int, bit_value: int, time: float) -> None:
         """Add a change to the bit vector, maintaining sorted order."""
         self._changes.append((bit_index, bit_value, time))
         self._changes.sort(key=lambda x: x[2])
@@ -93,6 +11,102 @@ class WaveGen:
     def changes(self):
         """Get the list of changes."""
         return self._changes.copy()
+    
+    @property
+    def wave_vector(self):
+        return self.WaveVector(self.changes)
+
+    @property
+    def bus_vector(self):
+        return self.BusVector(self.changes)
+
+    
+    class BusVector:
+        def __init__(self, changes):
+            self.changes = changes
+            self.bus_vector = self._generate()
+            
+        def __getitem__(self, index):
+            return self.bus_vector[index]
+
+        def __str__(self):
+            return '\n'.join([f"Time:  {time:5d}, Mask: {mask:032b}" for time, mask in self.bus_vector])
+
+        def __len__(self):
+            return len(self.bus_vector)
+        
+        def _generate(self):
+            changes = sorted(self.changes, key=lambda x: x[2])
+            bus_vector = []
+            current_time = 0
+            current_mask = 0
+            for bit_index, bit_value, time in changes:
+                delay = time - current_time
+                if delay < 0:
+                    raise ValueError("Time of change is less than current time")
+                else:
+                    bus_vector.append((current_time, current_mask))
+                current_time = time
+                if bit_value:
+                    current_mask |=  (1 << bit_index)
+                else:
+                    current_mask &= ~(1 << bit_index)
+                    
+            # Include the final state at the end of simulation, after processing all changes.
+            bus_vector.append((current_time, current_mask))
+                    
+            return bus_vector
+
+
+    class WaveVector:
+        def __init__(self, changes):
+            self.changes = sorted(changes, key=lambda x: x[2])
+            self.wave_vector = self._generate()
+
+        def __str__(self):
+            return '\n'.join([f"Delay: {delay:5d},  Set: {set:032b}, Clear: {clr:032b}" for set, clr, delay in self.wave_vector])
+        
+        def __getitem__(self, index):
+            return self.wave_vector[index]
+
+        def __len__(self):
+            return len(self.wave_vector)
+
+        def _generate(self):
+            wave_vector = []
+            prev_time = 0
+            set_mask = 0
+            clr_mask = 0
+
+            # Iterate over sorted changes
+            for bit_index, bit_value, current_time in self.changes:
+                # Calculate delay since last change
+                delay = current_time - prev_time
+                
+                # If delay < 0, the changes are not properly sorted.
+                if delay < 0:
+                    raise ValueError("Time of change is less than current time")
+                
+                # If there was a delay, record it and reset the masks
+                if delay > 0:
+                    wave_vector.append((set_mask, clr_mask, delay))
+                    set_mask = 0
+                    clr_mask = 0
+
+                # Update masks based on the bit value
+                if bit_value:
+                    set_mask |= (1 << bit_index)
+                else:
+                    clr_mask |= (1 << bit_index)
+
+                # Update previous time
+                prev_time = current_time
+
+            # Include the final change if any mask is set
+            if set_mask or clr_mask:
+                wave_vector.append((set_mask, clr_mask, 0))
+
+            return wave_vector
 
 
 import unittest
@@ -100,47 +114,38 @@ import unittest
 class TestWaveGen(unittest.TestCase):
     def test_changes_ordered(self):
         wavegen = WaveGen()
-
         wavegen.change_bit(0, 1, 10)
         wavegen.change_bit(1, 0, 20)
         wavegen.change_bit(2, 1, 30)
         wavegen.change_bit(3, 0, 40)
-
-        self.assertEqual(wavegen.changes, [(0, 1, 10), (1, 0, 20), (2, 1, 30), (3, 0, 40)])
-
-    def test_bus_vector_generation(self):
-        wavegen = WaveGen()
-
-        wavegen.change_bit(0, 1, 10)
-        bus_vector = BusVector(wavegen.changes)
-
-        expected_result = [(0, 0b0), (10, 0b1)]
-        self.assertEqual(bus_vector.generate(), expected_result)
+        expected_result = [(0, 1, 10), (1, 0, 20), (2, 1, 30), (3, 0, 40)]
+        self.assertEqual(wavegen.changes, expected_result)
 
     def test_out_of_order_changes_handled(self):
         wavegen = WaveGen()
-
         wavegen.change_bit(3, 1, 35)
         wavegen.change_bit(0, 1, 10)
         wavegen.change_bit(3, 0, 40)
         wavegen.change_bit(1, 1, 5)
         wavegen.change_bit(2, 1, 30)
         wavegen.change_bit(1, 0, 20)
+        expected_result = [(1, 1, 5), (0, 1, 10), (1, 0, 20), (2, 1, 30), (3, 1, 35), (3, 0, 40)]
+        self.assertEqual(wavegen.changes, expected_result)
 
-        self.assertEqual(wavegen.changes, [(1, 1, 5), (0, 1, 10), (1, 0, 20), (2, 1, 30), (3, 1, 35), (3, 0, 40)])
+    def test_bus_vector_generation(self):
+        wavegen = WaveGen()
+        wavegen.change_bit(0, 1, 10)
+        expected_result = [(0, 0b0), (10, 0b1)]
+        self.assertEqual(wavegen.bus_vector[:], expected_result)
 
     def test_wave_vector_generation(self):
         wavegen = WaveGen()
-
         wavegen.change_bit(1, 0, 20)
         wavegen.change_bit(0, 1, 10)
         wavegen.change_bit(3, 0, 40)
         wavegen.change_bit(3, 1, 35)
         wavegen.change_bit(1, 1, 5)
         wavegen.change_bit(2, 1, 30)
-
-        wave_vector = WaveVector(wavegen.changes)
-        
         # Compare the delays and the set_mask, clr_mask
         expected_result = [
             (0b0000, 0b0000,  5),  # Initial delay
@@ -151,8 +156,7 @@ class TestWaveGen(unittest.TestCase):
             (0b1000, 0b0000,  5),  # (3, 1, 35)
             (0b0000, 0b1000,  0),  # (3, 0, 40)
         ]
-        generated_result = [(set_mask, clr_mask, delay) for set_mask, clr_mask, delay in wave_vector.generate()]
-        self.assertEqual(generated_result, expected_result)
+        self.assertEqual(wavegen.wave_vector[:], expected_result)
         
 
 # Run the tests when the script is executed
